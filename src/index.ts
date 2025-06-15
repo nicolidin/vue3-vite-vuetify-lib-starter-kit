ec637c9d53684146114560fe9ffb44731c9322a5
nicolas <nicolas.lidin@gmail.com>
2025-06-15 16:30:01 +0200

    feat: clean code

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,17 @@
-import MozaicArticles from "./components/Molecules/Mozaic/MozaicArticles/MozaicArticles.vue";
-import ArticlePreview from "./components/Molecules/ArticlePreview/ArticlePreview.vue";
 import MarkdownText from "./components/Molecules/MarkdownText/MarkdownText.vue";
-import ArticleMarkdownReadonly from "./components/Molecules/ArticleMardownReadonly/ArticleMardownReadonly.vue";
-import NoteCreation from "./components/Molecules/NoteCreation/NoteCreation.vue";
-import type {NoteCreated} from "./components/Molecules/NoteCreation/NoteCreation.vue"
-
 import Layout from "./components/Organisms/Layout/Layout.vue";
-import ListReactive from "./components/Molecules/ListReactive.vue";
 import { DEFAULT_VUETIFY_CONFIG } from "./lidinAppKitConfig/vuetifyConfig/defaultVuetifyConfig";
 import { createLidinAppKit } from "./lidinAppKitConfig/createLidinAppKit.ts";
 import { useLanguage } from "./modules/globalAppData/composables/useLanguage.ts";
 import { useTheme } from "./modules/globalAppData/composables/useTheme.ts";
-import { useFormZodValidation } from "./composables/useFormValidation/useFormZodValidation.ts";
 import { generateRandomUuid } from "@/services/utils/randomId.ts";
 
 export {
   useLanguage,
   useTheme,
   createLidinAppKit,
-  useFormZodValidation,
   DEFAULT_VUETIFY_CONFIG,
-  ArticlePreview,
   MarkdownText,
-  MozaicArticles,
-  ArticleMarkdownReadonly,
-  NoteCreation,
-  NoteCreated,
   Layout,
-  ListReactive,
   generateRandomUuid,
 };
--- src/types/sizes.ts
+++ src/types/sizes.ts
@@ -1,6 +1,4 @@
 // 🔹 Type générique de tailles utilisées globalement
 export type SizeType = "small" | "medium" | "large" | "xlarge";
 
-export type ArticlePreviewSizeType = SizeType | "auto";
-
 export type MarkdownTextSize = "xsmall" | "small" | "medium" | "large";
